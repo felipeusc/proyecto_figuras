ecc1d1a573e18252c10637bc7e809497aeb1ee56
Felipe Manuel Aba González <felipemanuel.aba@rai.usc.es>
2024-09-26 21:45:47 +0200

    Creadas las funciones del triángulo

---
+++ lib/triangulo.py
@@ -0,0 +1,6 @@
+def get_identificador()->str:
+    return "Triangulo"
+def get_area(base:int,altura:int)->int:
+    return (base*altura)//2
+def get_perimetro(lado1:int,lado2:int,lado3:int)->int:
+    return lado1+lado2+lado3
